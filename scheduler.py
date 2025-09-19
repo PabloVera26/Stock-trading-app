354cd6751bea9df65c9b0f1bdddcc6b6b7985229
PabloVera26 <jahirvera400@gmail.com>
2025-09-18 22:52:31 -0600

    scheduler added to the app.

--- scheduler.py
+++ scheduler.py
@@ -3,5 +3,13 @@ import time
 from app import run_stock
 from datetime import datetime
 
-def basic_job():
+def log_job_start():
     print("Job started at:", datetime.now())
+
+schedule.every().minute.do(log_job_start)
+
+schedule.every().minute.do(run_stock)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
