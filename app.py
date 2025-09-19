1baba8dccecb1c179654d2c2bb9f57df38e1b55d
PabloVera26 <jahirvera400@gmail.com>
2025-09-18 22:37:05 -0600

    run_stock function created.

--- app.py
+++ app.py
@@ -3,42 +3,50 @@ import dotenv
 import os
 import csv
 
+
 dotenv.load_dotenv()
 LIMIT = 100
 
-POLYGON_APY_KEY = os.getenv("POLYGON_APY_KEY")
+POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
 
-url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_APY_KEY}"
+url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}"
 
-response = requests.get(url)
-tickers = response.json()
+def run_stock():
+    response = requests.get(url)
+    tickers = response.json()
 
-print(tickers)
+    tickers_list = []
 
-tickers_list = []
-for ticker in tickers["results"]:
-    tickers_list.append(ticker)
+    for ticker in tickers["results"]:
+        tickers_list.append(ticker)
 
-print(len(tickers_list))
+    while 'next_url' in tickers:
+        print('requesting next page', tickers['next_url'])
+        response = requests.get(tickers['next_url'] + f"&apiKey={POLYGON_API_KEY}")
+        tickers = response.json()
+        print(tickers)
+        for ticker in tickers["results"]:
+            tickers_list.append(ticker)
 
-csv_headers = [
-    'ticker', 'name', 'market', 'locale', 'primary_exchange', 
-    'type', 'active', 'currency_name', 'cik', 'composite_figi', 
-    'last_updated_utc'
-]
+    csv_headers = [
+        'ticker', 'name', 'market', 'locale', 'primary_exchange', 
+        'type', 'active', 'currency_name', 'cik', 'composite_figi', 
+        'last_updated_utc'
+    ]
 
-csv_filename = 'tickers.csv'
-with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=csv_headers)
-    writer.writeheader()
-    
-    for ticker_data in tickers_list:
-        row = {}
-        for header in csv_headers:
-            row[header] = ticker_data.get(header, '')
-        writer.writerow(row)
+    csv_filename = 'tickers.csv'
+    with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_headers)
+        writer.writeheader()
+        
+        for ticker_data in tickers_list:
+            row = {}
+            for header in csv_headers:
+                row[header] = ticker_data.get(header, '')
+            writer.writerow(row)
 
-print(f"Tickers data saved to {csv_filename}")
+    print(f"Tickers data saved to {csv_filename}")
+
 
 example_structure = {
     'ticker': 'ACHR.WS', 
@@ -53,3 +61,6 @@ example_structure = {
     'composite_figi': 'BBG00YGCV1N9', 
     'last_updated_utc': '2025-09-16T06:05:51.696609632Z'
 }
+
+if __name__ == "__main__":
+    run_stock()
